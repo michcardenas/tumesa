41f36800e441e23d72e0bcc85bf0ba9954ba5c9f
michael.cardenas <michael.cardenas@realtechltda.com>
2025-08-25 22:50:57 -0500

    ss

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use MercadoPago\MercadoPagoConfig;
+use MercadoPago\Client\Preference\PreferenceClient;
+use MercadoPago\Exceptions\MPApiException;
 
 
 class PagoController extends Controller
