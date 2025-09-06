bb45d441c7ec00667fa44661136067f053a4e258
michael.cardenas <michael.cardenas@realtechltda.com>
2025-09-06 18:42:01 -0500

    clase ingresos

--- app/Http/Controllers/IngresosController.php
+++ app/Http/Controllers/IngresosController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Cena;
 use Illuminate\Support\Facades\Log;
+use App\Models\Reserva;
+
 
 class IngresosController extends Controller
 {
