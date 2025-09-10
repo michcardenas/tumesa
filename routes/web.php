297b05a94b530f0c4b344771a04135b989bc4cc0
michael.cardenas <michael.cardenas@realtechltda.com>
2025-09-09 21:43:34 -0500

    s

--- routes/web.php
+++ routes/web.php
@@ -220,6 +220,8 @@ Route::get('/admin/cenas/create', [App\Http\Controllers\AdminCenasController::cl
 Route::post('/admin/cenas', [App\Http\Controllers\AdminCenasController::class, 'store'])->name('admin.cenas.store');
 Route::get('/admin/cenas/{cena}/edit', [App\Http\Controllers\AdminCenasController::class, 'edit'])->name('admin.cenas.edit');
 Route::delete('/admin/cenas/{cena}', [App\Http\Controllers\AdminCenasController::class, 'destroy'])->name('admin.cenas.destroy');
+Route::post('/admin/cenas', [App\Http\Controllers\AdminCenasController::class, 'store'])->name('admin.cenas.store');
+
 
 
 require __DIR__.'/auth.php';
